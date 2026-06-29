136b5d51d5b7bf18507ecbb0dac3a3fa214c9557
Yousef Alian <yousef227d@gmail.com>
2026-06-29 14:01:36 +0300

    Delete Downloads/DEEPRED-SEA-6ae3443bb459ee94461582a31cacc4dc2b434d38 directory

--- Downloads/DEEPRED-SEA-6ae3443bb459ee94461582a31cacc4dc2b434d38/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://divigo.runasp.net/api/v1'
-};
--- Downloads/DEEPRED-SEA-6ae3443bb459ee94461582a31cacc4dc2b434d38/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://divigo.runasp.net/api/v1',
-
-  centerApiUrl: 'http://divigo.runasp.net/api/v1'
-};
